ba37dbaf465d334e314e99de7d726d68cf81f0b1
Jordan Primas <jorprimas20@gmail.com>
2024-01-28 09:26:22 -0800

    completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+       # 1. Remove the first node from the `nodes_to_visit` list
+      node = nodes_to_visit.pop(0)
+      # 2. Check each id to see if it's equal to node['id'] and return that node else return None
+      if node['id'] == id:
+        return node 
+      nodes_to_visit = node['children'] + nodes_to_visit
+       # 3. Add its children (if any) to the BEGINNING of the `nodes_to_visit` list
+    return None
+
